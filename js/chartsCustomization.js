e7abb138e4d384e5b81f75efbd1e02a80a622ca4
ArshGamare <arsh.gamare@gmail.com>
2021-12-17 20:00:29 -0600

    Implement random function on all charts with button

--- js/chartsCustomization.js
+++ js/chartsCustomization.js
@@ -2,6 +2,8 @@ Chart.defaults.plugins.legend.display = false;
 Chart.defaults.scale.grid.display = false;
 Chart.defaults.scale.ticks.display = false;
 
+var size = [window.width,window.height]
+
 const plugin = {
     id: 'custom_canvas_background_color',
     beforeDraw: (chart) => {
@@ -14,14 +16,66 @@ const plugin = {
     }
 };
 
-let bubbleChart = document.getElementById('bubbleChart').getContext('2d');
-let mergeChart = document.getElementById('mergeChart').getContext('2d');
-let insertionChart = document.getElementById('insertionChart').getContext('2d');
-let selectionChart = document.getElementById('selectionChart').getContext('2d');
-let quickChart = document.getElementById('quickChart').getContext('2d');
-let countingChart = document.getElementById('countingChart').getContext('2d');
 
-let barChart0 = new Chart(bubbleChart, {
+var bubbleChart = document.getElementById('bubbleChart').getContext('2d');
+var mergeChart = document.getElementById('mergeChart').getContext('2d');
+var insertionChart = document.getElementById('insertionChart').getContext('2d');
+var selectionChart = document.getElementById('selectionChart').getContext('2d');
+var quickChart = document.getElementById('quickChart').getContext('2d');
+var countingChart = document.getElementById('countingChart').getContext('2d');
+
+var barChart0 = new Chart(bubbleChart, {
+    type: 'bar',
+    data:{
+        labels:['1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', ],
+        datasets:[{
+            data: [
+                1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24
+            ],
+            backgroundColor: '#ff5252',
+            barThickness: 5,
+            hoverBackgroundColor: 'black'
+        }]
+    },
+    plugins: [plugin],
+    options: {
+        tooltips: {
+            enabled: false
+        },
+        scales: {
+            yAxes: {
+                ticks: {
+                    display:true
+                }
+            }
+        }
+    }
+});
+
+var barChart1 = new Chart(mergeChart, {
+    type: 'bar',
+    data:{
+        labels:[1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24,  ],
+        datasets:[{
+            data: [ 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24],
+            backgroundColor: '#ff5252',
+            barThickness: 5,
+            hoverBackgroundColor: 'black'
+        }]
+    },
+    plugins: [plugin],
+    options: {
+        scales: {
+            yAxes: {
+                ticks: {
+                    display:true
+                }
+            }
+        }
+    }
+});
+
+var barChart2 = new Chart(insertionChart, {
     type: 'bar',
     data:{
         labels:['1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', ],
@@ -46,7 +100,7 @@ let barChart0 = new Chart(bubbleChart, {
     }
 });
 
-let barChart1 = new Chart(mergeChart, {
+var barChart3 = new Chart(selectionChart, {
     type: 'bar',
     data:{
         labels:['1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', ],
@@ -71,7 +125,7 @@ let barChart1 = new Chart(mergeChart, {
     }
 });
 
-let barChart2 = new Chart(insertionChart, {
+var barChart4 = new Chart(quickChart, {
     type: 'bar',
     data:{
         labels:['1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', ],
@@ -96,57 +150,7 @@ let barChart2 = new Chart(insertionChart, {
     }
 });
 
-let barChart3 = new Chart(selectionChart, {
-    type: 'bar',
-    data:{
-        labels:['1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', ],
-        datasets:[{
-            data: [
-                1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24
-            ],
-            backgroundColor: '#ff5252',
-            barThickness: 5,
-            hoverBackgroundColor: 'black'
-        }]
-    },
-    plugins: [plugin],
-    options: {
-        scales: {
-            yAxes: {
-                ticks: {
-                    display:true
-                }
-            }
-        }
-    }
-});
-
-let barChart4 = new Chart(quickChart, {
-    type: 'bar',
-    data:{
-        labels:['1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', ],
-        datasets:[{
-            data: [
-                1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24, 1, 38, 4, 12, 32, 2, 5, 67, 24
-            ],
-            backgroundColor: '#ff5252',
-            barThickness: 5,
-            hoverBackgroundColor: 'black'
-        }]
-    },
-    plugins: [plugin],
-    options: {
-        scales: {
-            yAxes: {
-                ticks: {
-                    display:true
-                }
-            }
-        }
-    }
-});
-
-let barChart5 = new Chart(countingChart, {
+var barChart5 = new Chart(countingChart, {
     type: 'bar',
     data:{
         labels:['1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', '1', '38', '4', '12', '32', '2', '5', '67', '24', ],
@@ -172,3 +176,33 @@ let barChart5 = new Chart(countingChart, {
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    randomizeDataset();
+}, false);
+
+function randomizeDataset() {
+    for(let i = 0; i < 250; i++) {
+        let x = Math.floor(Math.random()*1000);
+        barChart0.data.datasets[0].data[i] = x;
+        barChart1.data.datasets[0].data[i] = x;
+        barChart2.data.datasets[0].data[i] = x;
+        barChart3.data.datasets[0].data[i] = x;
+        barChart4.data.datasets[0].data[i] = x;
+        barChart5.data.datasets[0].data[i] = x;
+        barChart0.data.labels[i] = x;
+        barChart1.data.labels[i] = x;
+        barChart2.data.labels[i] = x;
+        barChart3.data.labels[i] = x;
+        barChart4.data.labels[i] = x;
+        barChart5.data.labels[i] = x;
+    }
+    barChart0.update();
+    barChart1.update();
+    barChart2.update();
+    barChart3.update();
+    barChart4.update();
+    barChart5.update();
+}
+function uploadDataset() {
+    //use map function to 
+}
